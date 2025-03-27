24ff68966549d644c4e10e2509c4c99065cf665e
iBadVan <ireano@unsa.edu.pe>
2025-03-27 14:44:34 -0500

    Update Cajoneria.java

--- Ejercicios/Cajoneria.java
+++ Ejercicios/Cajoneria.java
@@ -7,5 +7,16 @@ public class Cajoneria implements Iterable<Caja<?>>{
     private ArrayList<Caja<?>> lista;
     private int tope;
 
-    
+    public Cajoneria(int tope) {
+        this.tope = tope;
+        lista = new ArrayList<>();
+    }
+
+    public void add(Caja<?> caja) {
+        if (lista.size() < tope) {
+            lista.add(caja);
+        } else {
+            System.out.println("La cajonería está llena.");
+        }
+    }
 }
