17d46c785772e0bb01e1bdeb4ea4958f0bccd351
iBadVan <ireano@unsa.edu.pe>
2025-03-27 14:42:55 -0500

    Update Caja.java

--- Ejercicios/Caja.java
+++ Ejercicios/Caja.java
@@ -1,10 +1,22 @@
 package Ejercicios;
 
-public class Caja {
+public class Caja  <T> {
     private String color;
     private T contenido;
 
     public Caja(String color) {
         this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void guardar(T contenido) {
+        this.contenido = contenido;
+    }
+
+    public T obtenerContenido() {
+        return contenido;
     }
 }
