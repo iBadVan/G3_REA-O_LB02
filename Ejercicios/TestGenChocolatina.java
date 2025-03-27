d0ba7b66ebcb41cf7423cda29233ac4de9d1575f
iBadVan <ireano@unsa.edu.pe>
2025-03-27 15:32:40 -0500

    Update TestGenChocolatina.java

--- Ejercicios/TestGenChocolatina.java
+++ Ejercicios/TestGenChocolatina.java
@@ -2,6 +2,33 @@ package Ejercicios;
 
 public class TestGenChocolatina {
     public static void main(String[] args) {
-        
+        Cajoneria cajoneria = new Cajoneria(5);
+
+        Caja<Chocolatina> caja1 = new Caja<>("rojo");
+        caja1.guardar(new Chocolatina("Milka"));
+
+        Caja<Chocolatina> caja2 = new Caja<>("azul");
+        caja2.guardar(new Chocolatina("Ferrero"));
+
+        Caja<Chocolatina> caja3 = new Caja<>("verde");
+        caja3.guardar(new Chocolatina("Sublime"));
+
+        Caja<Chocolatina> caja4 = new Caja<>("amarillo");
+        caja4.guardar(new Chocolatina("Nestle"));
+
+        Caja<Chocolatina> caja5 = new Caja<>("rosado");
+        caja5.guardar(new Chocolatina("Kinder"));
+
+        cajoneria.add(caja1);
+        cajoneria.add(caja2);
+        cajoneria.add(caja3);
+        cajoneria.add(caja4);
+        cajoneria.add(caja5);
+
+        System.out.println("Cajoneria Inicial: ");
+        System.out.println(cajoneria);
+
+
+
     }
 }
