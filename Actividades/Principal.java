d4f23deae8f48370f58f45514c2b41b620972768
iBadVan <ireano@unsa.edu.pe>
2025-03-26 23:14:35 -0500

    golosinas

--- Actividades/Principal.java
+++ Actividades/Principal.java
@@ -12,5 +12,19 @@ public class Principal {
         for (Chocolatina chocolatina: bolsaCho) {
             System.out.println(chocolatina.getMarca());
         } 
+
+        Bolsa <Golosina> bolsaGolo = new Bolsa <Golosina> (5);
+        Golosina g = new Golosina("chupetin",2);
+        Golosina g1 = new Golosina("caramelo",4);
+        Golosina g2 = new Golosina("paletita", 17);
+        bolsaGolo.add(g);
+        bolsaGolo.add(g1);
+        bolsaGolo.add(g2); 
+        for (Golosina golosinita: bolsaGolo) {
+            System.out.println(golosinita.getNombre());
+            System.out.println(golosinita.getPeso());
+        } 
+
+
     }
 }
