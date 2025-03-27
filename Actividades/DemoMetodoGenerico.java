83f771069119039aefc8b9cffa6db58c2e6f1ad5
iBadVan <ireano@unsa.edu.pe>
2025-03-26 22:54:44 -0500

    Update DemoMetodoGenerico.java

--- Actividades/DemoMetodoGenerico.java
+++ Actividades/DemoMetodoGenerico.java
@@ -1,19 +1,16 @@
 package Actividades;
 
 public class DemoMetodoGenerico {
-    static <T extends Comparable<T>> boolean igualArrays (T[] x, T[] y){
-        //Si las longitudes de los array son diferentes,
-        // entonces los array son diferentes
-        if (x.length != y.length){
+    static <T extends Comparable<T>> boolean igualArrays(T[] x, T[] y) {
+        // Si las longitudes de los arrays son diferentes, no son iguales
+        if (x.length != y.length)
             return false;
-        }
 
-        for (int i = 0; i < x.length; i++){ 
-            if(!x[i].equals(y[i])){
-                return false;
-            }else{
-                return true; 
-            }       
+        for (int i = 0; i < x.length; i++) {
+            if (!x[i].equals(y[i]))
+                return false; 
         }
-    }       
+        return true; 
+    }     
+    
 }
