7653c3da4643956e930aded9239ca2e637725e27
iBadVan <ireano@unsa.edu.pe>
2025-03-26 22:55:34 -0500

    Update DemoMetodoGenerico.java

--- Actividades/DemoMetodoGenerico.java
+++ Actividades/DemoMetodoGenerico.java
@@ -17,6 +17,13 @@ public class DemoMetodoGenerico {
         Integer nums2[]={1,2,3,4,5};
         Integer nums3[]={1,2,7,4,5};
         Integer nums4[]={1,2,7,4,5,6};
-        
+        if (igualArrays(nums,nums))
+            System.out.println("nums es igual a nums");
+        if (igualArrays(nums,nums2))
+            System.out.println("nums es igual a nums2");
+        if (igualArrays(nums,nums3))
+            System.out.println("nums es igual a num3");
+        if (igualArrays(nums,nums4))
+            System.out.println("nums es igual a nums4");
     }
 }
